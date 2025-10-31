b44da1d479fed0798448f1c4e9d8615c3618a410
Vladimir Fomin <fomin.v97@yandex.ru>
2025-10-31 15:17:42 +0300

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+/** @type {import('prettier').Config} */
+const config = {
+  printWidth: 100,
+  trailingComma: 'all',
+  semi: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  arrowParens: 'always',
+  htmlWhitespaceSensitivity: 'css',
+  jsxSingleQuote: true,
+  proseWrap: 'never',
+  quoteProps: 'as-needed',
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+};
+
+export default config;
