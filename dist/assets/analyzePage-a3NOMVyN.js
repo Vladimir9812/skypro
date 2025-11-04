ceec5f9d996289c82eb3de096c522b1a968b8dd6
Vladimir Fomin <fomin.v97@yandex.ru>
2025-11-04 20:41:27 +0300

    Настройка базового пути

--- dist/assets/analyzePage-a3NOMVyN.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-C_y3qir0.js";import{W as s,L as n}from"./widgetLayout-BUzokGVq.js";import{t as e}from"./text-22NOqvX5.js";const o="_content_1wq0j_1",a={content:o},r=()=>t.jsx(s,{children:t.jsx("div",{className:a.content,children:e.widget.empty})}),m=()=>t.jsx(n,{title:e.title.analyzeExpenses,children:t.jsx(r,{})});export{m as default};
--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  base: '/skypro/',
   plugins: [react(), eslint(), svgr()],
   css: {
     postcss: {
